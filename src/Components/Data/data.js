85222eb49aa4ac1f16b221178781edbf25ee941c
Tabish Javed <Tabishjaved2030@gmail.com>
2025-08-12 12:48:07 +0530

    project sequence

--- src/Components/Data/data.js
+++ src/Components/Data/data.js
@@ -1,6 +1,16 @@
 const projects = {
     allProject: [
         {
+            image: '/codecompete-screenshot.png',
+            title: 'CodeCompete | LIVE Site available',
+            description: 'CodeCompete is an innovative web application designed for competitive programmers who want to improve their skills, compete with friends, and track their rankings. Features include topic-wise contests, local & global rankings, dedicated practice mode, custom groups, exciting rewards, flexible contest timing, unique question sets, post-contest practice, and discussion forum. Built with Vite, JavaScript, Tailwind CSS, MUI.',
+            sourcecode: 'https://github.com/tabish-27/CodeCompete.git',
+            livelink: 'https://code-compete-v.vercel.app/',
+            teckstack: ["React.js", "Tailwind CSS", "MUI"],
+            status: 'LIVE',
+            featured: true
+        },
+         {
             image: '/food-zaika-screenshot.png',
             title: 'Food Zaika | LIVE Site available',
             description: 'Food Zaika is a modern, responsive food delivery web application built with React and Tailwind CSS. Features include real-time search, dark/light theme toggle, cart management with Redux, location-based restaurant discovery, infinite scroll, and PWA support. Experience seamless food ordering with a beautiful UI and mobile-first design.',
@@ -17,16 +27,6 @@ const projects = {
             sourcecode: 'https://github.com/tabish-27/PicLingo.git',
             livelink: 'https://pic-lingo.vercel.app/',
             teckstack: ["React", "Tailwind CSS", "Node.js", "Express.js", "MongoDB", "CLIP AI", "Machine Learning" ],
-            status: 'LIVE',
-            featured: true
-        },
-        {
-            image: '/codecompete-screenshot.png',
-            title: 'CodeCompete | LIVE Site available',
-            description: 'CodeCompete is an innovative web application designed for competitive programmers who want to improve their skills, compete with friends, and track their rankings. Features include topic-wise contests, local & global rankings, dedicated practice mode, custom groups, exciting rewards, flexible contest timing, unique question sets, post-contest practice, and discussion forum. Built with Vite, JavaScript, Tailwind CSS, MUI.',
-            sourcecode: 'https://github.com/tabish-27/CodeCompete.git',
-            livelink: 'https://code-compete-v.vercel.app/',
-            teckstack: ["React.js", "Tailwind CSS", "MUI"],
             status: 'LIVE',
             featured: true
         },
